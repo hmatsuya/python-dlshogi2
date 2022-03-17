895b4d1cf004c569e25a46cf9cc0d99e7f797d96
Hiroaki Matsuyama <hmatsuya@gmail.com>
2022-03-17 18:14:03 +0900

    Fix setup.py for pydlshogi2.features

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 ﻿from setuptools import setup, find_namespace_packages, find_packages
 
-setup(name='python-dlshogi2', packages=find_namespace_packages(include=['pydlshogi2.*']))
+setup(
+    name='python-dlshogi2',
+    packages=find_packages(where='pydlshogi2'),
+    package_dir={"": "pydlshogi2"}
+)
